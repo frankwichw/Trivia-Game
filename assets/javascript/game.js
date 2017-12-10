8e89614fe21fc4ea905f641594e1e759d611bb37
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2017-12-09 22:40:00 -0500

    Work in progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var userGuess;
+
+var trivia = {
+	questions: [
+	"What year was Neopets created?",
+	""],
+	answersOne: [
+	],
+	answersTwo: [
+	],
+	answersThree: [
+	]
+};
+// pseudocode: 
+// function to begin displays start button, div created by javascript. on click it hides itself, begins game:
+// game begins by adding text to various divs:
+// 1. time remaining div
+// 2. question div
+// 3. creates answer divs from an array of answers for that question
+// begins countdown that decrements time remaining div until it = 0
+// buttons onclick if/else statement in which it sees if the button clicked matches answer, by saying if user clicks button 2 (for instance), set userGuess = 2
+// if it does then enter certain text into the divs saying correct answer, and increment the correct answers variables
+
+$("#startButton").on("click", function(){
+
+});
+
+function firstQuestion(){
+
+};
+
+function secondQuestion(){
+
+};
+
+function thirdQuestion(){
+
+};
