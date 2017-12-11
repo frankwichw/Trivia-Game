c7fd520b34e28d77bab4a4cc58c3218c90271b42
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2017-12-11 15:31:57 -0500

    WIP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,69 @@
+$( document ).ready(function() {
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var userGuess;
 
 var trivia = {
 	questions: [
-	"What year was Neopets created?",
-	""],
+	"Question one?",
+	"Question two?",
+	"Question three?"
+	],
 	answersOne: [
+	"Right answer",
+	"Wrong answer",
+	"Wrong answer",
+	"Wrong answer"
 	],
 	answersTwo: [
+	"Wrong answer",
+	"Wrong answer",
+	"Right answer",
+	"Wrong answer"
 	],
 	answersThree: [
+	"Wrong answer",
+	"Wrong answer",
+	"Right answer",
+	"Wrong answer"
 	]
 };
-// pseudocode: 
-// function to begin displays start button, div created by javascript. on click it hides itself, begins game:
-// game begins by adding text to various divs:
-// 1. time remaining div
-// 2. question div
-// 3. creates answer divs from an array of answers for that question
-// begins countdown that decrements time remaining div until it = 0
-// buttons onclick if/else statement in which it sees if the button clicked matches answer, by saying if user clicks button 2 (for instance), set userGuess = 2
-// if it does then enter certain text into the divs saying correct answer, and increment the correct answers variables
 
-$("#startButton").on("click", function(){
+	$("#startButton").click(function(){
+		firstQuestion();
+	});
+
+
+	function firstQuestion(){
+		var time = 30;
+		$("#startButton").addClass("hide");
+		$("#timeRemaining").html("<h2>" + time + "</h2>");
+		$("#question").removeClass("hide");
+		$("#question").html("<h3>" + trivia.questions[0] + "</h3>");
+		$("#buttonOne").removeClass("hide");
+		$("#buttonTwo").removeClass("hide");
+		$("#buttonThree").removeClass("hide");
+		$("#buttonFour").removeClass("hide");
+		$("#buttonOne").html(trivia.answersOne[0]);
+		$("#buttonTwo").html(trivia.answersOne[1]);
+		$("#buttonThree").html(trivia.answersOne[2]);
+		$("#buttonFour").html(trivia.answersOne[3]);
+		setInterval(function(){
+			time--;
+			$("#timeRemaining").html("<h2>" + time + "</h2>");
+		}, 1000);
+		$("#buttonOne").click(function(){
+			correctAnswers++;
+			
+		});
+	};
+
+	function secondQuestion(){
+
+	};
+
+	function thirdQuestion(){
+
+	};
 
 });
-
-function firstQuestion(){
-
-};
-
-function secondQuestion(){
-
-};
-
-function thirdQuestion(){
-
-};
